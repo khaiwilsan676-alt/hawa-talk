0028de53411fb1279f859ea3e358c613ce9675de
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-07-23 12:38:24 +0530

    Update LoginPage.tsx

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -3,7 +3,8 @@
 import { useState } from 'react'
 import { Phone } from 'lucide-react'
 import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from "../lib/firebase";
+// ⬇️ FIXED IMPORT LINE: Directly pointing to the index file inside the firebase folder
+import { auth, provider } from "../lib/firebase/index";
 
 interface LoginPageProps {
   onLoginSuccess?: (data?: any) => void
@@ -156,5 +157,4 @@ export default function LoginPage({ onLoginSuccess }: LoginPageProps) {
       </div>
     </div>
   )
-}
-
+        }
