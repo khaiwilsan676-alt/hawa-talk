9a4e9b9a41b6b14bab47cc8a522dfa145404b24f
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-07-24 16:04:45 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hawa.app',
+  appName: 'Hawa',
+  webDir: 'out',
+};
+
+export default config;
