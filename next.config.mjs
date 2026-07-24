85a84c2c53bd4050d28a0f1ab1df7e2474cd3106
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-07-24 15:57:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   distDir: 'out', // Explicitly set output directory to 'out'
   images: {
     unoptimized: true, // Next.js images static export ke liye
