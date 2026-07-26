636983837f11c186037ed75268d34fa8eb08083a
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-07-26 16:46:32 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.hawa.app',
   appName: 'Hawa',
   webDir: 'out',
-  server: {
-    url: 'https://jb-hm.vercel.app', // Yaha apna actual Vercel wala link daal de
-    cleartext: true
-  }
 };
 
 export default config;
-
