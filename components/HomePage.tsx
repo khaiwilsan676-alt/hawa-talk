7efef41f53ab64957034d134602c37d591a454c9
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-08-20 14:31:08 +0530

    Adjust marginTop for popular tab rendering

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1736,7 +1736,7 @@ const renderMineTab = () => (
   const renderPopularTab = () => (
     <>
       <div className="px-4" style={{ 
-        marginTop: '-55px', 
+        marginTop: '-85px', 
         position: 'relative', 
         zIndex: 10 
       }}>
