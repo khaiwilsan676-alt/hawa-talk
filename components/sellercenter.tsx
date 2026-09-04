e7e5ea67d9417912beb6ce5aa011f12805b8d160
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-09-05 00:08:28 +0530

    Fix import statement in sellercenter.tsx

--- components/sellercenter.tsx
+++ components/sellercenter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'interface';
+import React, { useEffect, useState } from 'react';
 
 interface SellerCenterProps {
   onBack?: () => void;
