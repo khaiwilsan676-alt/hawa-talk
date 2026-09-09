0caf288e86ce2db402b9b25656f3fb36c73d60cf
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-09-10 00:52:03 +0530

    Update winners array size to last 13 entries

--- components/Fruitparty.tsx
+++ components/Fruitparty.tsx
@@ -412,7 +412,7 @@ export default function Fruitparty({ onClose }: FruitpartyProps) {
         
         setBets({});
         setProcessedRound(gameState.round);
-        setWinners(w => [...w, winnerImgToSave].slice(-10));
+        setWinners(w => [...w, winnerImgToSave].slice(-13));
         
         if (totalBetThisRound > 0) {
           setRoundHistory(prev => [{ 
