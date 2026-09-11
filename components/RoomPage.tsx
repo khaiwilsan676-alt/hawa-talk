410837672cbebc9cd3fdd10953513c7ea0eef9a1
khaiwilsan676-alt <khaiwilsan676@gmail.com>
2026-09-12 01:58:11 +0530

    Increase seat size for smaller screens

--- components/RoomPage.tsx
+++ components/RoomPage.tsx
@@ -1903,7 +1903,7 @@ function RoomContent({ roomOwner, currentUser, onClose, onBack, onKeepRoom, onFo
 
         @media (max-width: 400px) {
           :root {
-            --seat-size: 65px;
+            --seat-size: 75px;
             --header-btn-size: 38px;
             --header-icon-size: 22px;
             --header-room-img-size: 38px;
